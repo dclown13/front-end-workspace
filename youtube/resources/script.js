bbe1594b1247ae1f102279b76347f3fd8faff997
dclown13 <w6092w@gmail.com>
2023-09-11 13:04:40 +0900

    09-11

---
+++ youtube/resources/script.js
@@ -0,0 +1,8 @@
+const asideIcon = document.querySelector("#aside-icon");
+const aside = document.querySelector("main");
+
+asideIcon.addEventListener('click', function() {
+    // 스타일 변경!
+    aside.classList.toggle("aside-change");
+});
+
